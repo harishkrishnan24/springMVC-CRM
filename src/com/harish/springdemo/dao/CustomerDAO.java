8b7733430eb7ae5544290a0dd2dfa89869f013c3
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-08-24 21:43:44 +0530

    Add DAO, view and entity for customer

---
+++ src/com/harish/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.harish.springdemo.dao;
+
+import java.util.List;
+
+import com.harish.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
